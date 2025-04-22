f293eb029f139f2dfb428330090aecc23bb249ba
TheLudway <theludway@gmail.com>
2025-04-21 22:41:47 -0500

    Lista enlazada en C++

---
+++ C++/LinkedList/Implementaciones/SimpleLinkedList.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+
+using namespace std;
+
+
+class Node{
+    public:
+        int data;
+        Node* next;
+
+        Node(int val){
+            data = val;
+            next = nullptr;
+        }
+};
+
+class LinkedList {
+    private:
+        Node* head;
+
+    public:
+        LinkedList(){
+            head = nullptr;
+        }
+
+        void insert(int val){
+            Node* newNode = new Node(val);
+
+            if (head == nullptr){
+                head = newNode;
+            }else{
+                Node* temp = head;
+                while (temp -> next != nullptr)
+                    temp = temp -> next;
+
+                temp -> next = newNode;
+            }
+        }
+
+        void display(){
+            Node* temp = head;
+
+            while (temp != nullptr){
+                cout << temp->data << " -> " ;
+                temp = temp -> next;
+            }
+            cout << "NULL" << endl;
+        }
+
+        void deleteValue(int val){
+            if (head == nullptr) return;
+
+            if (head -> data == val){
+                Node* toDelete = head;
+                head = head -> next;
+                delete toDelete;
+                return;
+            }
+
+            Node* temp = head;
+            while (temp -> next != nullptr && temp -> next -> data != val)
+                temp = temp -> next;
+
+            if (temp -> next == nullptr){
+                cout << "No encontrado" << endl;
+                return;
+            }
+
+            Node* toDelete = temp -> next;
+            temp -> next = temp -> next -> next;
+            delete toDelete;
+        }
+
+};
+
+int main() {
+    LinkedList list;
+
+    list.insert(10);
+    list.insert(20);
+    list.insert(30);
+
+    cout << "Lista enlazada: " ;
+    list.display();
+
+    cout << "Eliminar elemento 20" << endl;
+    list.deleteValue(20);
+
+    cout << "Lista actualizada: ";
+    list.display();
+
+    return 0;
+}
+
+    

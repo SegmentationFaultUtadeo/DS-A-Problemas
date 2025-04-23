b65ad89e138fb7cb5afc908985afa97f4b6ec1c9
VanegasMora <vanegas.mora23@gmail.com>
2025-04-23 16:14:53 -0500

    Base de ListaEnlazada en JS

--- JavaScript/HolaMundo.js
+++ JavaScript/HolaMundo.js
@@ -1,1 +1,24 @@
-console.log("Hola Mundo!");
+//Clase nodo
+
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor(head = null){
+        this.head = head
+    }
+}
+
+
+let node1 = new Node(20)
+let node2 = new Node(30)
+node1.next = node2
+
+let list1 = new LinkedList(node1)
+
+console.log(list1)
+console.log(node2)

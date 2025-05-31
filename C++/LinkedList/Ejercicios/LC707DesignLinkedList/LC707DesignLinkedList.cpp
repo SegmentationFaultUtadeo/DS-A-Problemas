1ebd15279736b734fae62c6260a1f00c998b36ce
TheLudway <theludway@gmail.com>
2025-05-31 10:45:38 -0500

    Otro intento para ver lo del ejercicio LC707

--- C++/LinkedList/Ejercicios/LC707DesignLinkedList/LC707DesignLinkedList.cpp
+++ C++/LinkedList/Ejercicios/LC707DesignLinkedList/LC707DesignLinkedList.cpp
@@ -15,71 +15,12 @@ class Node{
 class MyLinkedList{
     public:
         Node* head;
+        int length_list;
         MyLinkedList(){
             head = nullptr;
+            length_list = 0;
         }
 
-        int get(int index){
-            int i = 0;
-            Node* iter = head;
-
-            while (i < index && iter -> next != nullptr){
-                i++;
-                iter = iter -> next;
-            }
-            if (iter == nullptr || index < 0)
-                return -1;
-            return iter -> val;
-        }
-
-        void addAtHead(int val){
-            Node* nuevoNode = new Node(val);
-            nuevoNode -> next = head;
-            head = nuevoNode;
-        }
-
-        void addAtTail(int val){
-            Node* iter = head;
-            if (iter == nullptr){
-                addAtHead(val);
-                return;
-            }
-            Node* nuevoNode = new Node(val);
-            while (iter -> next != nullptr)
-                iter = iter -> next;
-            iter -> next = nuevoNode;
-        }
-
-        void addAtIndex(int index, int val){
-            if (index == 0){
-                addAtHead(val);
-                return;
-            }
-            Node* iter = head;
-            Node* nuevoNode = new Node(val);
-            for(int i = 0; i < index - 1; i++)
-                iter = iter -> next;
-            Node* temp = iter -> next;
-            iter -> next = nuevoNode;
-            nuevoNode -> next = temp;
-        }
-
-        void deleteAtIndex(int index){
-            Node* iter = head;
-            if (index < 0) return;
-            if (index == 0){
-                Node* temp = head -> next;
-                delete head;
-                head = temp;
-                return;
-            }
-            for (int i=0; i < index-1; i++)
-                iter = iter -> next;
-            if (iter == nullptr) return;
-            Node* temp = iter -> next -> next;
-            delete iter -> next;
-            iter -> next = temp;
-        }
 
         void printList(){
             Node* iter = head;
@@ -97,40 +38,7 @@ class MyLinkedList{
 int main(){
 
     MyLinkedList* obj = new MyLinkedList();
-//     ["MyLinkedList","addAtHead","deleteAtIndex","addAtHead","addAtHead","addAtHead","addAtHead","addAtHead","addAtTail","get","deleteAtIndex","deleteAtIndex"]
-// [[],[2],[1],[2],[7],[3],[2],[5],[5],[5],[6],[4]]
 
-    obj -> addAtHead(2);
-    obj -> printList();
-
-    obj -> deleteAtIndex(1);
-    obj -> printList();
-
-    obj -> addAtHead(2);
-    obj -> printList();
-
-    obj -> addAtHead(7);
-    obj -> printList();
-
-    obj -> addAtHead(3);
-    obj -> printList();
-
-    obj -> addAtHead(2);
-    obj -> printList();
-
-    obj -> addAtHead(5);
-    obj -> printList();
-
-    obj -> addAtTail(5);
-    obj -> printList();
-
-    int in {obj -> get(5)};
-
-    obj -> deleteAtIndex(6);
-    obj -> printList();
-
-    obj -> deleteAtIndex(4);
-    obj -> printList();
 
 
 

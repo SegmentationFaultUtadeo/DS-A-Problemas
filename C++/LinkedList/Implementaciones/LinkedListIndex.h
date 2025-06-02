fe6c306718712ed3f7a1e82121327c0ff30f1bd7
TheLudway <theludway@gmail.com>
2025-06-01 22:17:17 -0500

    Se agrega un header para una implementación de la lista enlazada pero utilizando unos métodos diferentes TODO: falta documentación

--- C++/LinkedList/Implementaciones/LinkedListIndex.h
+++ C++/LinkedList/Implementaciones/LinkedListIndex.h
@@ -1,29 +1,35 @@
+#ifndef LINKED_LIST_INDEX_H
+#define LINKED_LIST_INDEX_H
+
 #include <bits/stdc++.h>
-#define endl "\n"
 
 using namespace std;
 
+template<typename T>
 class Node{
     public:
-        int val;
+        T data;
         Node* next;
-        Node(int data){
-            val = data;
+        Node(T val){
+            data = val;
             next = nullptr;
         }
 };
 
-class MyLinkedList{
-    public:
-        Node* head;
+template<typename T>
+class LinkedListIndex{
+    private:
+        Node<T>* head;
         int length_list;
-        MyLinkedList(){
+
+    public:
+        LinkedListIndex(){
             head = nullptr;
             length_list = 0;
         }
 
         void printList(){
-            Node* iter = head;
+            Node<T>* iter = head;
             while (iter != nullptr){
                 cout << iter -> val << " -> ";
                 iter = iter -> next;
@@ -31,20 +37,19 @@ class MyLinkedList{
             cout << "NULL" << endl;
         }
 
-        int get(int index){
+        T get(int index){
 
             if (index < 0 || index >= length_list) return -1;
 
-            Node* iter = head;
+            Node<T>* iter = head;
 
             for (int i = 0; i < index; i++) iter = iter -> next;
 
             return iter -> val;
-
         }
 
-        void addAtHead(int val){
-            Node* newNode = new Node(val);
+        void addAtHead(T val){
+            Node<T>* newNode = new Node(val);
 
             if (head == nullptr){
                 head = newNode;
@@ -57,41 +62,40 @@ class MyLinkedList{
             length_list++;
         }
 
-        void addAtTail(int val){
+        void addAtTail(T val){
 
             if (head == nullptr){addAtHead(val); return;};
 
-            Node* newNode = new Node(val);
-            Node* iter = head;
+            Node<T>* newNode = new Node(val);
+            Node<T>* iter = head;
 
-            while (iter -> next != nullptr) iter = iter -> next;
+            while(iter -> next != nullptr) iter = iter -> next;
 
             iter -> next = newNode;
             length_list++;
         }
 
-        void addAtIndex(int index, int val){
+        void addAtIndex(int index, T val){
 
             if (index > length_list) return;
 
             if (index == length_list) {addAtTail(val); return;};
 
-            Node* newNode = new Node(val);
-            Node* iter = head;
+            Node<T>* newNode = new Node(val);
+            Node<T>* iter = head;
 
             for (int i = 0; i < index - 1; i++) iter = iter -> next;
 
             newNode -> next = iter -> next;
             iter -> next = newNode;
             length_list++;
-
         }
 
         void deleteAtIndex(int index){
             if (index >= length_list) return;
 
             if (index == 0){
-                Node* temp = head;
+                Node<T>* temp = head;
                 head = temp -> next;
                 length_list--;
                 delete temp;
@@ -99,10 +103,10 @@ class MyLinkedList{
                 return;
             }
 
-            Node* iter = head;
-            for (int i = 0; i < index -1 ; i++) iter = iter -> next;
+            Node<T>* iter = head;
+            for (int i = 0; i < index - 1; i+=) iter = iter -> next;
 
-            Node* temp = iter -> next;
+            Node<T>* temp = iter -> next;
             iter -> next = temp -> next;
             length_list--;
             delete temp;
@@ -110,42 +114,4 @@ class MyLinkedList{
         }
 };
 
-
-
-
-int main(){
-
-    MyLinkedList* obj = new MyLinkedList();
-
-    cout << "agregar tres elementos" << endl;
-    obj -> addAtHead(1);
-    obj -> addAtHead(2);
-    obj -> addAtTail(3);
-    obj -> printList();
-
-    cout << "Len de la lista" << endl;
-    cout << obj -> length_list << endl;
-
-    cout << "Quitar el elemento del indice 1" << endl;
-    obj -> deleteAtIndex(1);
-    obj -> printList();
-
-    cout << "Len de la lista" << endl;
-    cout << obj -> length_list << endl;
-
-    cout << "Agregar al final y eliminar al final" << endl;
-    obj -> addAtTail(4);
-    obj -> printList();
-    obj -> deleteAtIndex(2);
-    obj -> printList();
-
-
-    cout << "Quiero obtener el valor del indice 1" << endl;
-    obj -> addAtTail(4);
-    obj -> printList();
-    int i1 {obj -> get(1)};
-    cout << i1 << endl;
-
-
-    return 0;
-}
+#endif

1de80630eaf2f9931e10e0cb5e40648f10fac279
TheLudway <theludway@gmail.com>
2025-06-03 17:54:01 -0500

    Kattis

---
+++ C++/Problemas/KStatistics/KStatistics.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main(){
+
+    int n, i = 0;
+    while (cin >> n){
+        int max =  -1e7;
+        int min = 1e7;
+        while (n--){
+            int n2;
+            cin >> n2;
+            if (n2 > max) max = n2;
+            if (n2 < min) min = n2;
+        }
+        cout << "Case " << ++i << ": " << min << " " << max << " " << (max - min) << endl << endl;
+    }
+
+
+    return 0;
+}

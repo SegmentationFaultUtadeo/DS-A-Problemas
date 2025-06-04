fccea0a32862ec8a3d9b01aae015fa5222fc2b42
TheLudway <theludway@gmail.com>
2025-06-03 20:09:03 -0500

    Ejercicio Etruscan Warriors Never Play Chess

--- C++/Problemas/UVa11614EtruscanWarriorsNeverPlayChess/UVa11614EtruscanWarriorsNeverPlayChess.cpp
+++ C++/Problemas/UVa11614EtruscanWarriorsNeverPlayChess/UVa11614EtruscanWarriorsNeverPlayChess.cpp
@@ -1,12 +1,18 @@
 #include <bits/stdc++.h>
 
 #define endl "\n"
+#define ll long long
 
 using namespace std;
 
 int main(){
 
-    sqrt()
+    int t; cin >> t;
+    while (t--){
+        ll n; cin >> n;
+        ll k = floor((-1 + sqrt(1 + 8.0*n))/2);
+        cout << k << endl;
+    }
 
 
     return 0;

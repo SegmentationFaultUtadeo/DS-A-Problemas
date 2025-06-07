1ae8671266ce943084c4518c641d69e0053be139
TheLudway <theludway@gmail.com>
2025-06-07 12:53:00 -0500

    Pilas y colas problema 2 reestructurado

--- C++/Queue/Ejercicios/SPPilasColasP2/SPPilasColasP2.cpp
+++ C++/Queue/Ejercicios/SPPilasColasP2/SPPilasColasP2.cpp
@@ -2,19 +2,15 @@
 #define endl "\n"
 
 
-Node* null_fun(){
-    return nullptr;
-}
 
-void reverse_at_k_2(Queue cola, int k){
-    if (k < 0 | k > cola.get_size() | cola.is_empty() == true) {
-        cout << "NULL" << endl;
-        return;
-    }
-    Node* iter = cola.front() -> next;
-    Node* prev = cola.front();
+void reverse_at_k_2(Queue<int> cola, int k){
+
+    if (k < 0 | k > cola.get_size() | cola.is_empty() == true) { cout << "NULL" << endl; return;}
+
+    Node<int>* iter = cola.front() -> next;
+    Node<int>* prev = cola.front();
     for (int i =0; i < k-1; i++){
-        Node* post = iter -> next;
+        Node<int>* post = iter -> next;
         iter -> next = prev;
         prev = iter;
         iter = post;
@@ -27,7 +23,7 @@ void reverse_at_k_2(Queue cola, int k){
 
 
 int main(){
-    Queue cola;
+    Queue<int> cola;
     cola.enqueue(10);
     cola.enqueue(-20);
     cola.enqueue(30);
@@ -39,7 +35,7 @@ int main(){
     // cola.enqueue(9);
     // cola.enqueue(10);
     cola.display();
-    reverse_at_k_2(cola, -3);
-    // cola_2.display();
+    reverse_at_k_2(cola, 3);
+    // cola.display();
 }
 

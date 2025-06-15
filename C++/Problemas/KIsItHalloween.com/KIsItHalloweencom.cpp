4e6a17aa95056a48650584660884b9f77c964c2b
TheLudway <theludway@gmail.com>
2025-06-15 11:17:54 -0500

    Ejercicio de Kattis IsItHalloween.com

---
+++ C++/Problemas/KIsItHalloween.com/KIsItHalloweencom.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main(){
+
+    string in; getline(cin, in);
+    cout << (((in == "OCT 31") || (in == "DEC 25")  ) ? "yup\n" : "nope\n");
+
+    return 0;
+}

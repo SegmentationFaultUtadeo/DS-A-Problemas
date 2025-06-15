fbd6143e77088a2a137c935025501a6127cc0547
TheLudway <theludway@gmail.com>
2025-06-14 22:23:19 -0500

    Ejercicio de UVa 10055 Hashmat the brave warrior

---
+++ C++/Problemas/UVa10055HashmatTheBraveWarrior/UVa10055HasmatTheBraveWarrior.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define endl "\n"
+
+#define ull unsigned long long
+
+using namespace std;
+
+
+int main(){
+
+    ull n1, n2; while (cin >> n1 >> n2) cout << (llabs(n2-n1)) << endl;
+
+
+    return 0;
+}

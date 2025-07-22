0a0bcec285aa1ece332de3ab3fefbf20079fecd4
TheLudway <theludway@gmail.com>
2025-07-21 20:13:18 -0500

    Problema de TexQuotes en C++

---
+++ C++/Problemas/UVaOnlineJudge/00272TeXQuotes/00272TeXQuotes.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+
+int main(){
+    string s;
+    while(getline(cin, s)){
+
+        bool first = true;
+        for (unsigned long i = 0; i < s.size(); i++){
+            s.replace
+
+        }
+    }
+
+    return 0;
+}

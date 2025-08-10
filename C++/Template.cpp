e13fae295817059199aa7d1e2d1f35c97dc38b55
TheLudway <theludway@gmail.com>
2025-08-10 18:17:06 -0500

    Modificar Template

--- C++/Template.cpp
+++ C++/Template.cpp
@@ -1,13 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define fast ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
 #define endl "\n"
+#define pb push_back
+#define all(x) (x).begin(). (x).end()
+#define sz(x) (int)(x).size()
+
 typedef long long ll;
 typedef short int si;
-#define fast ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+typedef unsigned long long ull;
+typedef long double ld;
+typedef unsigned int ui;
+typedef string str;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+
+
 
 int main(){
     fast;
-
 
     return 0;
 }

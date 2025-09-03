eea96cc1c9c330d48cdfefe5292cb1a63864bdf7
TheLudway <theludway@gmail.com>
2025-09-03 17:52:55 -0500

    Eso

---
+++ C++/Problemas/Kattis/StringMatching/StringMatching.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+
+using namespace std;
+
+
+const int p = 53;
+const int M = 1e9+9;
+
+vector<int> comproveP(int n){
+    vector<int> p (n, 0);
+}
+
+
+int main(){
+
+
+
+
+    return 0;
+}

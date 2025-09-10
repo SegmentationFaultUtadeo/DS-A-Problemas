8fe1c50290d23c11b50b2c0a40cf0d6e49f889ab
TheLudway <theludway@gmail.com>
2025-09-09 20:07:34 -0500

    MaximumEvenSum

---
+++ C++/Problemas/CodeForces/MaximumEvenSum/MaximumEvenSum.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+typedef long long ll;
+
+
+
+
+int main(){
+
+    int t; cin >> t;
+    while (t--){
+        ll a, b; cin >> a >> b;
+
+        vector<int> div;
+        for(int i =)
+
+
+
+    }
+
+
+}

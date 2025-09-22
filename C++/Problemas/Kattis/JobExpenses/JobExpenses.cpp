23b890dd9ece110daa24bc4e18dda33482500510
TheLudway <theludway@gmail.com>
2025-09-21 21:41:05 -0500

    Job

--- C++/Problemas/Kattis/JobExpenses/JobExpenses.cpp
+++ C++/Problemas/Kattis/JobExpenses/JobExpenses.cpp
@@ -3,7 +3,13 @@
 using namespace std;
 
 int main(){
-
+    int N; cin >> N;
+    int sol = 0;
+    while(N--){
+        int ki; cin >> ki;
+        sol += (ki < 0) ? abs(ki) : 0;
+    }
+    cout << sol << endl;
 
 return 0;
 }

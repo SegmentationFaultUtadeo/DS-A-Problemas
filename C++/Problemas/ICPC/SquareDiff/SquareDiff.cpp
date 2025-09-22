5a2e68d95a2684d4b524f7dd537adaa4aad1c798
TheLudway <theludway@gmail.com>
2025-09-21 21:02:51 -0500

    square diff

--- C++/Problemas/ICPC/SquareDiff/SquareDiff.cpp
+++ C++/Problemas/ICPC/SquareDiff/SquareDiff.cpp
@@ -10,8 +10,6 @@ void answer(ll N){
     char ans = 'N';
     for (ll x = 0; x <= N; ++x){
         ll y = sqrt(abs(x*x - N));
-        // cout << "x: " << x <<  " x2: " << x*x << endl;
-        // cout << "y: " << y << " y2: " << y*y << endl;
         if (x*x - y*y == N) {
             ans = 'Y';
             break;

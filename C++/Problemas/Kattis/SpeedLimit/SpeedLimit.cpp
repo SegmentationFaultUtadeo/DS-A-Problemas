7f800f508f24b6857b3a8ad423c54d97e64c9fb9
TheLudway <theludway@gmail.com>
2025-09-25 14:01:41 -0500

    CPP SpeedLimit

--- C++/Problemas/Kattis/SpeedLimit/SpeedLimit.cpp
+++ C++/Problemas/Kattis/SpeedLimit/SpeedLimit.cpp
@@ -7,9 +7,13 @@ int main(){
     si n; cin >> n;
 
     while (n != -1){
+        si distance = 0, t_prev = 0;
         while(n--){
             si s, t; cin >> s >> t;
+            distance += (t - t_prev) * s;
+            t_prev = t;
         }
+        cout << distance << " miles" << endl;
         cin >> n;
     }
 

4124b2a20786b69bf6181bc800517d74fe100213
TheLudway <theludway@gmail.com>
2025-09-26 14:07:00 -0500

    Python Template CP

--- Python/Template.py
+++ Python/Template.py
@@ -1,4 +1,17 @@
-n = int(input())
-while (n != 0):
-    n-=1;
-    # Hacer cosas
+def isOn(S, j):
+    return (S & ( 1 << j ))
+
+def setBits(S, j):
+    return (S | (1 << j))
+
+def clearBits(S, j):
+    return (S & ~(1 << j))
+
+def toggleBits(S, j):
+    return (S ^ (1 << j))
+
+def lowBits(S):
+    return (S & (-S))
+
+def setAll(n):
+    return ((1<<n) - 1)

bae221360c996540c38b2f0868dde89aa73dc6e3
TheLudway <theludway@gmail.com>
2025-10-01 16:11:08 -0500

    Problema 11991EasyProblemFromRujiaLiu en C++

---
+++ C++/Problemas/UVaOnlineJudge/11991EasyProblemFromRujiaLiu/11991EasyProblemFromRujiaLiu.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sz(x) (int)(x).size()
+#define endl '\n';
+
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+
+int  main (){
+    int n, m;
+    while(cin>>n>>m){
+        vector<vii> AL(1000000, vii());
+        for(int i = 0; i < n;++i){
+            int temp; cin >> temp;
+            AL[temp].push_back({temp, i});
+        }
+        while(m--){
+            int k, v;
+            cin >> k >> v;
+            if((int)AL[v].size() >= k){
+                cout<< AL[v][k-1].second+1 << endl;
+            }
+            else{
+                cout<<0 << endl;
+            }
+        }
+    }
+    return 0;
+}
---
+++ C++/Problemas/UVaOnlineJudge/11991EasyProblemFromRujiaLiu/README.md
@@ -0,0 +1,3 @@
+# 11991 Easy Problem from Rujia Liu?
+
+https://onlinejudge.org/external/119/11991.pdf

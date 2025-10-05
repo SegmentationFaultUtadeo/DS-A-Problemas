ce4fda181a4cd8aa02015b010618354c5ad2ac2a
TheLudway <theludway@gmail.com>
2025-10-04 23:55:10 -0500

    Ejercicio de Kattis BitByBit

--- C++/Problemas/Kattis/BitByBit/BitByBit.cpp
+++ C++/Problemas/Kattis/BitByBit/BitByBit.cpp
@@ -2,11 +2,40 @@
 #define endl '\n'
 using namespace std;
 
-
+typedef short int si;
 
 int main(){
-    int i = INT_MAX;
-    cout << "base 10: " << i << " base 2: " << bitset<31>(i) << endl;
+
+    si n; cin >> n;
+    while (n){
+        vector<char> bits(32, '?');
+        while(n--){
+            string instruction; cin >> instruction;
+            cin.get();
+            if (instruction == "SET"){
+                si i; cin >> i;
+                bits[i] = '1';
+            } else if (instruction == "CLEAR"){
+                si i; cin >> i;
+                bits[i] = '0';
+            } else if (instruction == "OR"){
+                si i, j; cin >> i >> j;
+                if (bits[i] == '1' || bits[j] == '1') bits[i] = '1';
+                else if (bits[i] == '?' || bits[j] == '?') bits[i] = '?';
+                else bits[i] = '0';
+            } else if (instruction == "AND"){
+                si i, j; cin >> i >> j;
+                if (bits[i] == '0' || bits[j] == '0') bits[i] = '0';
+                else if (bits[i] == '?' || bits[j] == '?') bits[i] = '?';
+                else bits[i] = (char)(bits[i]&bits[j]);
+            }
+
+
+        }
+        for(int i = 31; i >=0; --i) cout << bits[i];
+        cout << endl;
+        cin >> n;
+    }
 
     return 0;
 }

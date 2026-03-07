4fd86350f8cd94e44f6396321d0f72ce661aa1c3
TheLudway <theludway@gmail.com>
2026-03-07 14:55:17 -0500

    Humbertov

---
+++ C++/Problemas/HackerRank/HumbertovMoralovSequence/HumbertovMoralovSequence.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    long long n;
+    while (cin >> n && n != 0){
+        cout << (ceill(sqrtl(n))) << endl;
+    }
+        
+
+    return 0;
+}
